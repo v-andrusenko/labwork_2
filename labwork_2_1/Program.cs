8771fbe3b98f71dbfe47d9e01403d6bb8b8b33a8
v-andrusenko <andrusenko.vp1@gmail.com>
2021-02-15 15:28:41 +0200

    first commit

---
+++ labwork_2_1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace labwork_2_1
+{
+    class MyList<T>
+    {
+        private T[] myList;
+
+        public T this[int index]
+        {
+            get { return myList[index]; }
+            set { myList[index] = value; }
+        }
+
+        public MyList()
+        {
+            this.myList = new T[1];
+        }
+
+        public MyList(int count)
+        {
+            this.myList = new T[count];
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize(ref myList, myList.Length+1);
+            myList[myList.Length - 1] = item;
+        }
+
+        public int ArrayLength
+        {
+            get
+            {
+                return (myList.Length);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите изначальное количество элементов: ");
+            int num1;
+            do
+            {
+                num1 = int.Parse(Console.ReadLine());
+                if (num1 < 0) Console.Write("Введите корректное количество (больше или равно 0): ");
+            }
+            while (num1 < 0);
+            MyList<int> mylist = new MyList<int>(num1);
+            Random rand = new Random();
+
+            for (int i=0; i<num1;i++)
+            {
+                mylist[i] = rand.Next(0,10);
+            }
+            Console.WriteLine($"Общее количество элементов: {mylist.ArrayLength}");
+            Console.Write("Введите элемент, который нужно добавить: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            mylist.Add(num2);
+            Console.WriteLine($"Общее количество элементов после добавления: {mylist.ArrayLength}");
+            for (int i = 0; i < mylist.ArrayLength; i++)
+            {
+                Console.WriteLine($"Элемент номер {i}: {mylist[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ labwork_2_2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace labwork_2_2
+{
+    class MyDictionary <K, V>
+    {
+        private K[] DictionaryKeys;
+        
+        private V[] DictionaryValues;
+
+        public MyDictionary()
+        {
+            DictionaryKeys = Array.Empty<K>();
+            DictionaryValues = Array.Empty<V>();
+        }
+
+
+        public void Add(K key, V value)
+        {
+            Array.Resize(ref DictionaryKeys, DictionaryKeys.Length+1);
+            DictionaryKeys[DictionaryKeys.Length - 1] = key;
+
+            Array.Resize(ref DictionaryValues, DictionaryValues.Length + 1);
+            DictionaryValues[DictionaryValues.Length - 1] = value;
+        }
+
+        public V this[K key]
+        {
+            get
+            {
+                int num = Convert.ToInt32(key);
+                int index = Convert.ToInt32(DictionaryKeys[num - 1]);
+                return DictionaryValues[index];
+            }
+        }
+        public int DictionaryLength
+        {
+            get
+            {
+                return (DictionaryValues.Length);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+            myDictionary.Add(1, "Январь");
+            myDictionary.Add(2, "Февраль");
+            myDictionary.Add(3, "Март");
+            myDictionary.Add(4, "Апрель");
+            myDictionary.Add(5, "Май");
+            myDictionary.Add(6, "Июнь");
+            myDictionary.Add(7, "Июль");
+            myDictionary.Add(8, "Август");
+            myDictionary.Add(9, "Сентябрь");
+            myDictionary.Add(10, "Октябрь");
+            myDictionary.Add(11, "Ноябрь");
+            myDictionary.Add(12, "Декабрь");
+
+            Console.Write("Введите номер месяца: ");
+            int num;
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            while (num <= 0 || num > 13);
+            Console.WriteLine($"{num} - это {myDictionary[num-1]}");
+            Console.WriteLine($"\nОбщее количество пар: {myDictionary.DictionaryLength}");
+        }
+    }
+}
