6679b5018f91fd522c963dc4fa831fe39b368e1e
v-andrusenko <andrusenko.vp1@gmail.com>
2021-02-22 21:13:26 +0200

    update

---
+++ labwork_2_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace labwork_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForExample forexample = MyClass<ForExample>.FacrotyMethod();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyClass<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+    class ForExample
+    {
+        public ForExample()
+        {
+            Console.WriteLine("Порождение экземпляра типа ForExample");
+        }
+    }
+}
